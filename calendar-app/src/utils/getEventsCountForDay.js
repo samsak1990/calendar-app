33df08d7cd44524e1f2453a7e0e61f582d1747ef
samsak1990 <samsak.developer@internet.ru>
2025-06-12 07:27:32 +0300

    A little fises

--- calendar-app/src/utils/getEventsCountForDay.js
+++ calendar-app/src/utils/getEventsCountForDay.js
@@ -1,14 +1,13 @@
 export const getEventsCountForDay = (date, reminders) => {
-    if (!date || !reminders || typeof reminders !== 'object') return 0;
+    if (!date || !reminders) return 0;
 
     const targetDate = date.toDateString();
-    const destRem = Object.values(reminders)[0]
+    const parseReminders = Object.values(reminders)[0]
     const arrayDay = []
-    for(let rem in destRem){
-        // console.log(destRem[rem])
-        if (!destRem[rem].reminder_on_datetime) return false;
-        const reminderDate = new Date(destRem[rem].reminder_on_datetime).toDateString();
-        if(reminderDate === targetDate) arrayDay.push(destRem[rem]) ;
+    for(let rem in parseReminders){
+        if (!parseReminders[rem].reminder_on_datetime) return false;
+        const reminderDate = new Date(parseReminders[rem].reminder_on_datetime).toDateString();
+        if(reminderDate === targetDate) arrayDay.push(parseReminders[rem]) ;
         
     }
     return arrayDay.length
